0146011e72d4b0103afa34dd62ecc36eb0a45db5
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-10-28 15:26:20 +0300

    Update page.dart

--- lib/app/modules/home/page.dart
+++ lib/app/modules/home/page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +13,65 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      backgroundColor: Color(0xff212936),
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+        child: SafeArea(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(vertical: 18,horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 200.0,
+                  child: const Text('تحويل العملة',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold
+                ),
+                  ),
+                ),
+                Expanded(child: Center(
+                  child:  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: "ادخل المبلغ",
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18,
+                            color: Colors.blueAccent
+                          )
+                        ),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        ),
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+
+                    ],
+                  ),
+                ))
+              ],
+            ) ,
+        ),
+        ),
+      ),
+
+    );
   }
 }
